4727b64b4db66957f4e1aaa8c1980e98cf8726ad
eydeveloper <yaschenko.e02@gmail.com>
2022-07-17 11:32:55 +0300

    Fixed import

--- server/src/core/utils/jwt.ts
+++ server/src/core/utils/jwt.ts
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import {UserResponse} from '../../components/auth/authController';
+import {UserResponse} from '../../components/auth/types';
 
 export const createJwtToken = (payload: UserResponse) => {
   return jwt.sign(payload, `${process.env.JWT_SECRET_KEY}`, {expiresIn: '1h'});
